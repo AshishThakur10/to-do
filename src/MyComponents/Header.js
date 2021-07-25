fe5706dae28ee55459701daf71c1a147e69c8561
AshishThakur10 <sendashishthakur@gmail.com>
2021-07-25 23:30:26 +0530

    header
    update header components added

---
+++ src/MyComponents/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+      
+        <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <span class="navbar-text">
+            Navbar text with an inline element
+          </span>
+        </div>
+      </nav>
+
+    )
+}
